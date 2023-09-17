3b6be6669a47802e513f334823aa9cae69f57abd
Ahmet YILDIRIM <ahmetyildirim@live.com>
2023-09-17 09:48:50 +0300

    unguard all models in admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(Auth::guard("admin")->check()) {
+            Model::unguard();
+        } 
     }
 }
