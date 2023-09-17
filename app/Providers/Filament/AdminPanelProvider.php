8b87dfd4d570f9feea8ecc8cac9a762c8a35f756
Ahmet YILDIRIM <ahmetyildirim@live.com>
2023-09-17 09:36:57 +0300

    filament panel configure

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,6 +24,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->authGuard("admin")
             ->id('admin')
             ->path('admin')
             ->login()
